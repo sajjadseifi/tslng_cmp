88b91ac9d2e6d0061f7c4205181a355ec2ce1e9d
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 02:03:30 +0330

    fixed nullable value token construct

--- src/token.ts
+++ src/token.ts
@@ -3,7 +3,7 @@ import { IPosition, IToken, TokenType } from './types'
 export class Token implements IToken {
   constructor(
     public type: TokenType,
-    public val: string,
+    public val?: string,
     public pos?: IPosition
   ) {}
 }
