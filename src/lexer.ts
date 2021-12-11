a4b122f2de94189d9a672f3648f5711d7e932ec6
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 23:13:29 +0330

    impliemtn skip whitespace and skip comment

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,8 +1,9 @@
 import { Lex } from './lex'
-import { ILex, TokenType } from './types'
+import { ILex, IToken, TokenType } from './types'
 import { ILexer, ILexerAtomata } from './types/lexer'
 import { TokenError, TokenTypeError } from './types/type'
 import { keywords, patterns } from './constants'
+import { WHITE_SPACE } from './constants/pattern'
 export class Lexer implements ILexer, ILexerAtomata {
   lex: ILex
   tmp: string
@@ -10,22 +11,36 @@ export class Lexer implements ILexer, ILexerAtomata {
     this.lex = new Lex(src)
     this.tmp = ''
   }
+  skip_white_space() {
+    //at first char
+    this.lex.get_char()
+
+    //check if white space loop infinit
+    while (WHITE_SPACE.test(this.lex.ch)) this.lex.get_char()
+
+    //while end ch dosnt white space
+    this.lex.un_get_char()
+  }
   prev_token(): TokenError {
     throw new Error('Method not implemented.')
   }
   next_token(): TokenError {
-    throw new Error('Method not implemented.')
+    this.skip_white_space()
+    return this.init()
   }
   init(): TokenError {
     this.lex.get_char()
     this.tmp = this.lex.ch
     let tok_type: TokenTypeError
+
     //get identifier
     if (patterns.APHABETIC.test(this.tmp)) tok_type = this.iden()
     //get real number
-    if (patterns.POINT.test(this.tmp)) tok_type = this.error5()
+    else if (patterns.POINT.test(this.tmp)) tok_type = this.error5()
     //get number
-    if (patterns.NUMERIC.test(this.tmp)) tok_type = this.num()
+    else if (patterns.NUMERIC.test(this.tmp)) tok_type = this.num()
+    //get special char
+    else if (patterns.SPECIAL_CHAR.test(this.tmp)) tok_type = this.spec1()
 
     //not founded tokens
     tok_type = this.error13()
@@ -38,6 +53,7 @@ export class Lexer implements ILexer, ILexerAtomata {
       }
 
     this.lex.un_get_char()
+    if (this.tmp == '--') return this.init()
     //Itoken
     return {
       type: tok_type,
@@ -46,7 +62,9 @@ export class Lexer implements ILexer, ILexerAtomata {
     }
   }
   comment_line(): void {
-    throw new Error('Method not implemented.')
+    this.lex.get_char()
+    while (patterns.NEW_LINE.test(this.lex.ch)) this.lex.get_char()
+    this.lex.get_char()
   }
   comment_star(): void {
     throw new Error('Method not implemented.')
@@ -81,28 +99,39 @@ export class Lexer implements ILexer, ILexerAtomata {
     return keywords.list.some((k) => k === this.tmp)
   }
   spec1(): TokenType {
-    const s2 = this.spec2()
+    const s2 = this.spec2(this.tmp)
+    //add for un-get-char in init
+    this.lex.get_char()
+
     if (s2 !== false) return s2
 
     return TokenType.TOKEN_SPEC1
   }
-  spec2(): TokenType | false {
+  spec2(c1: string): TokenType | false {
     this.lex.get_char()
-    if (this.tmp == '--') this.comment_line()
+    const str = c1 + this.lex.ch
+    if (str == '--') {
+      this.comment_line()
+      return TokenType.TOKEN_SPEC2
+    }
 
-    const s3 = this.spec3()
+    const s3 = this.spec3(str)
 
     if (s3 !== false) return s3
-    if (this.tmp == '==') return TokenType.TOKEN_SPEC2
 
+    //2 size tok
+    if (str == '==') return TokenType.TOKEN_SPEC2
+    //un get char to corrent position of spec1
     this.lex.un_get_char()
 
     return false
   }
-  spec3(): TokenType | false {
+  spec3(c2: string): TokenType | false {
     this.lex.get_char()
+    //3 size tok
     if (this.tmp == '===') return TokenType.TOKEN_SPEC3
 
+    //un get char to corrent position of spec2
     this.lex.un_get_char()
     return false
   }
