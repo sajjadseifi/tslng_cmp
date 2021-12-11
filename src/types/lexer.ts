b2681d34b84cb1d5e5c83da2fbeb9a42652e944f
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 19:40:44 +0330

    add prev_token & chars contsept

--- src/types/lexer.ts
+++ src/types/lexer.ts
@@ -4,24 +4,27 @@ import { LexerError } from './error'
 
 export interface ILex {
   src: string
+  chars: string
   current_pos: IPosition
   get_char(): string
   un_get_char(): string
+  clear_chars(): void
 }
 
 export interface ILexer {
-  next_token(): IToken
+  prev_token(): IToken | never
+  next_token(): IToken | never
 }
 
 export interface ILexerAtomata {
-  init(): void
+  init(): IToken | LexerError
   num(): IToken
   real(): IToken
   iden(): IToken
   keyword(): IToken
   spec1(): IToken
-  spec2(c1: string): IToken
-  spec3(c2: string): IToken
+  spec2(): IToken
+  spec3(): IToken
   error5(): LexerError
   error13(): LexerError
 }
