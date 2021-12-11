52bba4197a29ef7ddc01273f17cd4e81c42121d7
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 18:47:15 +0330

    add regex pattern for lexical analyzing

---
+++ src/constants/pattern.ts
@@ -0,0 +1,13 @@
+export const APHABETIC = /^[a-zA-Z]$/
+
+export const IDENTIFIER = /^[a-zA-z_][a-zA-z_0-9]*$/
+
+export const NUMERIC = /^[0-9][0-9]*$/
+
+export const NON_ZERO = /^[1-9][0-9]*$/
+
+export const WHITE_SPACE = /^(\n|\s|\0|\r|\f)$/
+
+export const SPECIAL_CHAR = /[^A-Za-z0-9]/
+
+export const POINT = /^\.$/
