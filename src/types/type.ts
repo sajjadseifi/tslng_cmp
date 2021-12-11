76484b710f264b285716ddc9b076f996f5f4ea88
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 22:00:06 +0330

    add tokken_type_error

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,9 +1,10 @@
-import { IToken } from '.'
+import { IToken, TokenType } from '.'
 import { LexerError } from './error'
 
 export type Value = string
 
 export type TokenError = IToken | LexerError
+export type TokenTypeError = TokenType | LexerError
 
 export type LenOne = string
 export type LenTow = string
