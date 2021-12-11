ff175fcd5b6c839730b51c3fb2d3c49393a0e92d
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 20:13:22 +0330

    impliment of  worming file

---
+++ src/lex.ts
@@ -0,0 +1,29 @@
+import { Position } from './pos'
+import { ILex, IPosition } from './types'
+
+export class Lex implements ILex {
+  ch = ''
+  length = 0
+  pos: IPosition
+  index = 0
+
+  constructor(public src: string) {
+    this.pos = new Position(0, 0)
+  }
+  private update_ch() {
+    this.ch = this.src[this.index]
+  }
+  get_char(): void {
+    this.index++
+    this.update_ch()
+  }
+
+  un_get_char(): void {
+    this.index--
+    this.update_ch()
+  }
+
+  clear_chars(): void {
+    this.ch = ''
+  }
+}
