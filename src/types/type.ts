ead48b419792c6df64503c3c4cd18e8a8ba7d2b7
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-11 20:57:57 +0330

    exported type module

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,1 +1,9 @@
+import { IToken } from '.'
+import { LexerError } from './error'
+
 export type Value = string
+
+export type TokenError = IToken | LexerError
+
+export type LenOne = string
+export type LenTow = string
