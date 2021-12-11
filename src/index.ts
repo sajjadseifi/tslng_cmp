91f043b339ff6d505b74db13e3a9e36f0f26f09d
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 02:14:49 +0330

    add test for lexical

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import { Lex } from './lex'
-import { ILex } from './types'
+import { Lexer } from './lexer'
+import { Token } from './token'
+import { TokenError, TokenType } from './types'
+import { ILexer } from './types/lexer'
 
 const main = () => {
   console.log('start main')
-  const lex: ILex = new Lex(`
+
+  const lex: ILexer = new Lexer(`
   
     function sajjad() returns Int:
       --is now comment comments
       val sajjad : Int = 10;
+      if(sajjad == 10)
+        sajjad = 50;
     end
   `)
-  while (lex.length != 0) {
-    lex.get_char()
+  let tok: TokenError
 
-    if (lex.eof) break
-
-    console.log(lex.ch)
+  while (!lex.finished) {
+    const tok: TokenError = lex.next_token()
+    console.log(tok)
   }
   console.log('end main')
 }
