745a6dbb1a6d8e52e33e4ba558441aa3e5e2ad57
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 01:02:07 +0330

    refactor name of LexerError to ILexerError

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,10 +1,10 @@
 import { IToken, TokenType } from '.'
-import { LexerError } from './error'
+import { ILexerError } from './error'
 
 export type Value = string
 
-export type TokenError = IToken | LexerError
-export type TokenTypeError = TokenType | LexerError
+export type TokenError = IToken | ILexerError
+export type TokenTypeError = TokenType | ILexerError
 
 export type LenOne = string
 export type LenTow = string
