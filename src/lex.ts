849326dce5d532128103a4472deb1d7adf18636b
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 04:09:38 +0330

    add check new line & white space

--- src/lex.ts
+++ src/lex.ts
@@ -18,39 +18,49 @@ export class Lex implements ILex {
     this.length = 0
     this.buffer = Buffer.alloc(1)
   }
-  private update_ch(offset: number) {
+  private update_ch() {
     const num = fs.readSync(this.fd, this.buffer, 0, 1, this.index)
     const ch = String.fromCharCode(this.buffer[0])
     if (num === 0) this.ch = undefined
     else this.ch = ch
   }
-  get_char(): void {
+  get_char(use_tmp = true): void {
     if (this.eof) {
       this.index = this.length
       return this.clear_chars()
     }
 
     this.index++
-    this.update_ch(0)
-    this.tmp += this.ch
+    this.update_ch()
+    if (use_tmp) this.tmp += this.ch
   }
 
-  get is_new_line(): boolean {
-    return patterns.NEW_LINE.test(this.ch!)
-  }
-  un_get_char(): void {
+  un_get_char(use_tmp = true): void {
     if (this.index < 1) {
       this.index = -1
       return this.clear_chars()
     }
     this.index--
-    this.update_ch(-1)
-    if (this.tmp.length > 0) this.tmp = this.tmp.substr(0, this.tmp.length - 1)
+    this.update_ch()
+    if (use_tmp && this.tmp.length > 0)
+      this.tmp = this.tmp.substr(0, this.tmp.length - 1)
   }
-
+  get is_new_line(): boolean {
+    return patterns.NEW_LINE.test(this.ch!)
+  }
   clear_chars(): void {
     this.ch = ''
     this.tmp = ''
+  }
+  skip_white_space(): void {
+    //at first char
+    this.get_char(false)
+
+    //check if white space loop infinit
+    while (patterns.WHITE_SPACE.test(this.ch!)) this.get_char(false)
+
+    //while end ch dosnt white space
+    this.un_get_char(false)
   }
   get eof(): boolean {
     return this.ch == undefined
