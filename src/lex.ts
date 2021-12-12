8c3b498236e136c08b56c05f92b2d6684d927f69
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 03:55:39 +0330

    use read file & change updare_ch design code

--- src/lex.ts
+++ src/lex.ts
@@ -1,36 +1,42 @@
 import { patterns } from './constants'
 import { Position } from './pos'
 import { ILex, IPosition } from './types'
+import fs from 'fs'
 
 export class Lex implements ILex {
-  ch: string
+  ch?: string
   length: number
   pos: IPosition
   index: number
+  private buffer: Buffer
   tmp: string
-  constructor(public src: string) {
-    this.length = src.length
+  constructor(public fd: number) {
     this.ch = ''
     this.index = -1
     this.pos = new Position(0, 0)
     this.tmp = ''
+    this.length = 0
+    this.buffer = Buffer.alloc(1)
   }
-  private update_ch() {
-    this.ch = this.src[this.index]
+  private update_ch(offset: number) {
+    const num = fs.readSync(this.fd, this.buffer, 0, 1, this.index)
+    const ch = String.fromCharCode(this.buffer[0])
+    if (num === 0) this.ch = undefined
+    else this.ch = ch
   }
   get_char(): void {
-    if (this.index >= this.length - 1) {
+    if (this.eof) {
       this.index = this.length
       return this.clear_chars()
     }
 
     this.index++
-    this.update_ch()
+    this.update_ch(0)
     this.tmp += this.ch
   }
 
   get is_new_line(): boolean {
-    return patterns.NEW_LINE.test(this.ch)
+    return patterns.NEW_LINE.test(this.ch!)
   }
   un_get_char(): void {
     if (this.index < 1) {
@@ -38,7 +44,7 @@ export class Lex implements ILex {
       return this.clear_chars()
     }
     this.index--
-    this.update_ch()
+    this.update_ch(-1)
     if (this.tmp.length > 0) this.tmp = this.tmp.substr(0, this.tmp.length - 1)
   }
 
@@ -47,6 +53,6 @@ export class Lex implements ILex {
     this.tmp = ''
   }
   get eof(): boolean {
-    return this.index >= this.length
+    return this.ch == undefined
   }
 }
