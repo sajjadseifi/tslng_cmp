b70b60f23fc637178b501c682ff72d37baf80f3a
Sajjad Seifi <s.09334497255@gmail.com>
2021-12-12 21:07:02 +0330

    add EOF keyword

--- src/constants/keyword.ts
+++ src/constants/keyword.ts
@@ -4,5 +4,5 @@ export const VAL = 'val'
 export const RETURNS = 'returns'
 export const RETURN = 'return'
 export const END = 'end'
-
+export const EOF = '\0'
 export const list = [FUNCTION, RETURNS, IF, VAL, RETURN, END]
